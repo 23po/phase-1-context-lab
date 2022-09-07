caa5dd513cacdd034a916bf42ccdafc4cbaaf7e7
codalo <ckodalo@gmail.com>
2022-09-07 04:00:48 +0300

    Assignment Completed

--- index.js
+++ index.js
@@ -1,4 +1,70 @@
 /* Your Code Here */
+function createEmployeeRecord ([firstName, familyName, title, payPerHour]) {
+        const timeInEvents = [];
+        const timeOutEvents = [];
+         return {firstName, familyName, title, payPerHour, timeInEvents, timeOutEvents}
+     
+     }
+
+     function createEmployeeRecords(array) {
+        const newArray = [];
+        for (let i = 0; i < array.length; i++) {
+            newArray.push(createEmployeeRecord(array[i]));
+        }
+        return newArray;
+    }
+    
+    function createTimeInEvent(dateTime) {
+        const dateTimeArray = dateTime.split(' ');
+        
+    
+        const newEvent = {type: 'TimeIn',
+        hour: parseInt(dateTimeArray[1], 10),
+        date: dateTimeArray[0] };
+       this.timeInEvents.push(newEvent);
+    
+       return this;
+    
+    
+    }
+    function createTimeOutEvent(dateTime) {
+        const dateTimeArray = dateTime.split(' ');
+    
+        const newEvent = {type: 'TimeOut',
+        hour: parseInt(dateTimeArray[1], 10),
+        date: dateTimeArray[0]    
+    };
+    
+        this.timeOutEvents.push(newEvent);
+        return this;
+    
+    }
+    function hoursWorkedOnDate (date) {
+        
+        let filteredtimeInEvent = this.timeInEvents.find(function(timeInEvent) { return timeInEvent.date === date })
+        let filteredtimeOutEvent = this.timeOutEvents.find(function(timeOutEvent) { return timeOutEvent.date === date})
+    
+       return (filteredtimeOutEvent.hour - filteredtimeInEvent.hour)/100
+        
+        //let timeIn = parseInt(filteredtimeInEvent.hour, 10);
+       //let timeOut = parseInt(filteredtimeOutEvent.hour, 10);
+       
+       return timeOut - timeIn; 
+    }
+    function wagesEarnedOnDate(date) {
+        const payRate = this.payPerHour;
+    
+        return hoursWorkedOnDate.call(this, date) * payRate
+    }
+
+    function calculatePayroll(employeeRecords) {
+        return  employeeRecords.reduce( function (previousValue, currentValue) { return previousValue + allWagesFor.call(currentValue)}, 0)
+    
+    }
+    function findEmployeeByFirstName(srcArray, firstName) {
+       return srcArray.find( function (e) {return e.firstName === firstName })
+    }
+
 
 /*
  We're giving you this function. Take a look at it, you might see some usage
@@ -20,4 +86,6 @@ const allWagesFor = function () {
 
     return payable
 }
+
+
 
